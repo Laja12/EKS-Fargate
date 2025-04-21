a1761e45c0c502be3dcf8c6a2388a626df789472
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-21 22:13:46 +0530

    Update outputs.tf

--- Infra/modules/eks-fargate/outputs.tf
+++ Infra/modules/eks-fargate/outputs.tf
@@ -9,3 +9,23 @@ output "cluster_endpoint" {
 output "cluster_certificate_authority_data" {
   value = aws_eks_cluster.fargate.certificate_authority.0.data
 }
+
+output "cluster_name" {
+  value = aws_eks_cluster.fargate.name
+  description = "The name of the EKS cluster"
+}
+
+output "cluster_role_arn" {
+  value = aws_iam_role.eks_cluster_role.arn
+  description = "The ARN of the EKS cluster role"
+}
+
+output "cluster_security_group_id" {
+  value = aws_security_group.eks_cluster_sg.id
+  description = "The ID of the EKS cluster security group"
+}
+
+output "kubernetes_version" {
+  value = var.kubernetes_version
+  description = "The Kubernetes version for the EKS cluster"
+}
