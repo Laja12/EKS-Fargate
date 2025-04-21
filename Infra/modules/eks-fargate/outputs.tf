87d3e702552a8c17ae9a204981044df80f53673b
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-22 03:40:25 +0530

    Update outputs.tf

--- Infra/modules/eks-fargate/outputs.tf
+++ Infra/modules/eks-fargate/outputs.tf
@@ -20,11 +20,6 @@ output "cluster_role_arn" {
   description = "The ARN of the EKS cluster role"
 }
 
-output "cluster_security_group_id" {
-  value = aws_security_group.eks_cluster_sg.id
-  description = "The ID of the EKS cluster security group"
-}
-
 output "kubernetes_version" {
   value = var.kubernetes_version
   description = "The Kubernetes version for the EKS cluster"
