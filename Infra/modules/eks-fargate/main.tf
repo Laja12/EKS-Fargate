7533a0229546a96b87637c203396fc69a9efc931
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-22 04:19:41 +0530

    Update main.tf

--- Infra/modules/eks-fargate/main.tf
+++ Infra/modules/eks-fargate/main.tf
@@ -19,19 +19,29 @@ resource "aws_eks_cluster" "fargate" {
   tags = {
     Name = var.cluster_name
   }
+
+  # Enable EKS logging here
+  logging {
+    cluster_logging {
+      enabled = true
+      types = [
+        "api",
+        "audit",
+        "authenticator",
+        "controllerManager",
+        "scheduler"
+      ]
+    }
+  }
 }
 
-resource "aws_eks_cluster_log_config" "fargate_logging" {
-  cluster_name = aws_eks_cluster.fargate.name
+resource "aws_cloudwatch_log_group" "eks_cluster_log_group" {
+  name_prefix = "/aws/eks/${var.cluster_name}"
+  retention_in_days = var.log_retention_days
 
-  enable_cluster_logging = true
-  types = [
-    "api",
-    "audit",
-    "authenticator",
-    "controllerManager",
-    "scheduler"
-  ]
+  tags = {
+    Name = "${var.cluster_name}-logs"
+  }
 }
 
 resource "aws_iam_role" "eks_cluster_role" {
