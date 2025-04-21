edb5cd1185139e716cb62cdd1cedb637b5479601
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-22 04:24:01 +0530

    Delete Infra/modules/cloudwatch directory

--- Infra/modules/cloudwatch/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_cloudwatch_log_group" "eks_cluster_log_group" {
-  name_prefix        = "/aws/eks/${var.cluster_name}"
-  retention_in_days  = var.log_retention_days
-
-  tags = {
-    Name = "${var.cluster_name}-logs"
-  }
-}
-
