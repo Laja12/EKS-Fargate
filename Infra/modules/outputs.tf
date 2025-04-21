70c4672e603b5467471426b53d30cce167f7b933
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-21 17:01:31 +0530

    Create outputs.tf

---
+++ Infra/modules/outputs.tf
@@ -0,0 +1,23 @@
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "cluster_id" {
+  value = module.eks_fargate.cluster_id
+}
+
+output "cluster_endpoint" {
+  value = module.eks_fargate.cluster_endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  value = module.eks_fargate.cluster_certificate_authority_data
+}
+
+output "cloudwatch_log_group_name" {
+  value = module.cloudwatch_eks_logging.log_group_name
+}
+
+output "eks_cluster_security_group_id" {
+  value = aws_security_group.eks_cluster.id
+}
